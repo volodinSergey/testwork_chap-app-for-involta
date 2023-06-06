bdc5f53b36e84611f58da870664705e13a2292c2
Volodin Sergey <volodin7ergey@icloud.com>
2023-06-06 15:14:29 +0300

    [docs/sleep.util.ts]: read docs to sleep function

--- src/shared/utils/sleep.util.ts
+++ src/shared/utils/sleep.util.ts
@@ -1,1 +1,6 @@
+/**
+ * @title Sleep function
+ * @description Used for artifical delay
+ * @param ms How many seconds to wait 
+ */
 export const sleep = async (ms: number) => new Promise(res => setTimeout(res, ms))
