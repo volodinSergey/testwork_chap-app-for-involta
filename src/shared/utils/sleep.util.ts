af94f3e76feac03fc83f5aadbb3f26713206bbff
POLINKA <polinaivanova1708@gmail.com>
2024-10-18 15:34:07 +0300

    grgr

--- src/shared/utils/sleep.util.ts
+++
@@ -1,6 +0,0 @@
-/**
- * @title Sleep function
- * @description Used for artifical delay
- * @param ms How many miliseconds to wait 
- */
-export const sleep = async (ms: number) => new Promise(res => setTimeout(res, ms))
